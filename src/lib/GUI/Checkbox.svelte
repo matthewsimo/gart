178e24d4f4873f5b55c4943e1510743252223762
Matthew Simo <matthew.a.simo@gmail.com>
2022-06-24 14:10:36 -0500

    Add Checkbox GUI component

---
+++ src/lib/GUI/Checkbox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let title: string;
+	export let value: boolean;
+</script>
+
+<div class="form-control w-full max-w-xs">
+	<label class="label cursor-pointer">
+		<span class="label-text">{title}</span>
+		<input type="checkbox" bind:checked={value} class="checkbox" />
+	</label>
+</div>
